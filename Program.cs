5c1a363ade2ffc93e0a8cdff80236feb912b1f53
Matheus <matheusluan04@gmail.com>
2026-05-08 22:44:15 -0300

    Conclusão de atividade

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("digite sua senha: ");
+string senha = Console.ReadLine()!;
+
+if (senha == "1234abcd")
+{
+    Console.Write("Acesso permitido");
+}
+
+else
+{
+    Console.Write("Acesso negado");
+}
+
